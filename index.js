6ad8e0e13ec876267c4700ef458462f64fda2adc
Timothy Quirk <timothyfquirk@gmail.com>
2018-11-02 15:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //create function that shouts
 function shout(string) {
   return string.toUpperCase()
@@ -26,3 +27,34 @@ function sayHiToGrandma(string) {
     return 'I love you, too.';
   }
 }
+=======
+//create function that shouts
+function shout(string) {
+  return string.toUpperCase()
+}
+//create function that whispers
+function whisper(string) {
+  return string.toLowerCase();
+}
+// create function that prints shouted word
+function logShout(string) {
+  console.log(shout(string));
+}
+// create function that prints whispered word
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+// create a function that calls shout function so Gma can hear
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+     return 'I can\'t hear you!';
+  } 
+  if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+  } 
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  }
+
+}
+>>>>>>> ae9143c4dca6d91cf44cff02be555bec202d57be
